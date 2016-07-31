afc3cc87fff787a650f2571019a0e4d96d8c3f44
Peter Jacobson <github email address>
2016-07-31 14:54:52 -0400

    basic framework

--- db/migrate/20140703140340_create_users.rb
+++
@@ -1,6 +0,0 @@
-class CreateUsers < ActiveRecord::Migration
-  def change 
- create_table(:users) do |t|
-      t.string :user, :null => false, :default => :admin
-    end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20140703140419) do
 
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.integer  "happiness"
+    t.integer  "tickets"
+    t.integer  "height"
+    t.integer  "nausea"
+    t.boolean  "admin",           default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
 end
